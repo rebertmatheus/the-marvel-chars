c3f68fd080a5de0c0fcb1f6af977ff5fff8de4ed
Rebert M. Teixeira <rebertmatheus.t@gmail.com>
2023-04-23 02:21:05 -0300

    Add Readme and .Env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:the_marvel_chars/src/config/serviceLocator/service_locator.dart';
 import 'package:the_marvel_chars/src/config/theme/color_scheme_base.dart';
@@ -8,10 +9,12 @@ import 'package:the_marvel_chars/src/config/theme/theme.dart';
 import 'package:the_marvel_chars/src/features/characters/presentation/controllers/characters_view_controller.dart';
 import 'package:the_marvel_chars/src/features/characters/presentation/views/characters_view.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   ServiceLocator.setupServiceLocator();
+
+  await dotenv.load(fileName: '.env');
 
   final brightness = SchedulerBinding.instance.platformDispatcher.platformBrightness;
   bool isDarkMode = brightness == Brightness.dark;
--- lib/src/data/datasources/remote/api.dart
+++ lib/src/data/datasources/remote/api.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:the_marvel_chars/src/data/datasources/interface/datasources.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,10 +9,10 @@ class API implements IDataSources {
   factory API() => _instance;
   API._internal();
 
-  final String URL = 'gateway.marvel.com';
-  final String API_TS = 'themarvelchars';
-  final String API_KEY = '31edab4db2dfa852ea35c0bef22bfe4f';
-  final String API_HASH = 'b2f9da3e803acb791374f984e6056994';
+  final String URL = dotenv.env['MARVEL_URL'] ?? '';
+  final String API_TS = dotenv.env['MARVEL_API_TS'] ?? '';
+  final String API_KEY = dotenv.env['MARVEL_API_KEY'] ?? '';
+  final String API_HASH = dotenv.env['MARVEL_API_HASH'] ?? '';
 
   void _addKeys(Map<String, dynamic> params) {
     params.addAll({
